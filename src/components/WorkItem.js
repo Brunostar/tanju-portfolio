931bcda24eb9c89eef030516a7b720dc4612f9f2
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-09-26 13:05:01 +0000

    Update 4 files

---
+++ src/components/WorkItem.js
@@ -0,0 +1,13 @@
+export default function WorkItem() {
+    return (
+        <div class="work-item">
+            <div class="work-item__image">
+                {/* <img src="../img/portfolio-03.png" alt="" /> */}
+            </div>
+            <div class="work-item__text">
+                <h3 class="work-item__title">Title</h3>
+                <p class="work-item__tools">tech, tools</p>
+            </div>
+        </div>
+    )
+}
